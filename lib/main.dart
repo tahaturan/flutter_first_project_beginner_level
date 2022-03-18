6b9764436bfcd8c86327b1b8e84618aba7b2ac00
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-03-18 18:30:21 +0300

    Center kullanimi

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
@@ -24,6 +25,7 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
@@ -31,46 +33,18 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-          margin: const EdgeInsets.all(10),
-          width: 200,
-          height: 200,
-          decoration: BoxDecoration(
-          color: Colors.red,
-          border: Border.all(
-            color: Colors.blue,
-            width: 5,
-          ),
-          borderRadius: const BorderRadius.all(Radius.circular(20)),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                
-                margin: const EdgeInsets.only(left: 0,top: 3,right: 2,bottom: 2),
-                width: 50,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  border: Border.all(
-                    color: Colors.white,
-                    width: 3,
-                  )
-                ),
-              ),
-              Container(width: 50,height: 50,color: Colors.orange, margin: const EdgeInsets.all(5),),
-            ],
-          ),
-          
-          ),
-          Container(width: 100,height: 100,color: Colors.blue,),
-        ],
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.red,
+              child:  Center(child: Text("merhaba")),
+            ),
+          ],
+        ),
       ),
     );
   }
