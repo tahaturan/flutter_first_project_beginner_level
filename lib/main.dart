2df3af7e37d1c7b392748a6cc693893cc29dc39f
Muhammed Taha TURAN <tahaaturann16@gmail.com>
2022-03-29 02:17:59 +0300

    BoxDecoration egzersizleri

--- lib/main.dart
+++ lib/main.dart
@@ -27,24 +27,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final String _image1 =
+      "https://www.ilgincgercek.com/wp-content/uploads/2018/05/ilginc-araba.jpg";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Align(
-        alignment: Alignment.bottomLeft,
-        
+      body: Center(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          
           children: [
             Container(
               width: 200,
               height: 200,
-              color: Colors.red,
-              child: Align(
+              decoration: BoxDecoration(
+                  color: Colors.amber,
+                  shape: BoxShape.rectangle,
+                  border: Border.all(color: Colors.blue, width: 3),
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(image: NetworkImage(_image1),
+                  fit: BoxFit.cover
+                  ),
+                  boxShadow:const [
+                    BoxShadow(
+                      color: Colors.red,
+                      offset: Offset(-20, 20),
+                      blurRadius: 5,
+                      blurStyle: BlurStyle.normal,
+                    ),
+                    BoxShadow(
+                      color: Colors.blue,
+                      offset: Offset(-10, 10),
+                      blurRadius: 5,
+                      blurStyle: BlurStyle.normal,
+                    ),
+                  ]
+                  ),
+              child: const Align(
                 alignment: Alignment.topCenter,
                 child: Text("merhaba"),
               ),
